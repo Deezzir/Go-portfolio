9323225ecae9695084dabd85b484306199d98f6a
Iurii Kondrakov <55366304+Deezzir@users.noreply.github.com>
2022-08-26 04:00:27 -0400

    test

--- main.go
+++ main.go
@@ -30,8 +30,8 @@ var auth *Singleton
 
 var server_port = "0.0.0.0:8080"
 
-var username = os.Getenv("EMAIL_USERNAME")
-var password = os.Getenv("EMAIL_PASSWORD")
+// var username = os.Getenv("EMAIL_USERNAME")
+// var password = os.Getenv("EMAIL_PASSWORD")
 var host = "smtp.gmail.com"
 var port = "587"
 
