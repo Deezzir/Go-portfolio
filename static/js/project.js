c4f9a377329564e54da0a8facde5716d25baa98d
Iurii Kondrakov <55366304+Deezzir@users.noreply.github.com>
2022-08-31 21:48:58 -0400

    handler fixes

--- static/js/project.js
+++ static/js/project.js
@@ -18,6 +18,10 @@ function defineProject(html) {
             this.filters.forEach(btn => { btn.addEventListener('click', () => { this._filterBtn.call(this, btn); }) });
         }
 
+        disconnectedCallback() {
+            this.filters.forEach(btn => { btn.removeEventListener('click', () => { this._filterBtn }) });
+        }
+
         _filterBtn(btn) {
             let id = btn.getAttribute('id');
             this.projectCards.forEach(card => {
