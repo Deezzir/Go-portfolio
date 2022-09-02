f4a9942733f83853892851f504d53b1b8ddfe476
Iurii Kondrakov <55366304+Deezzir@users.noreply.github.com>
2022-09-02 17:39:25 -0400

    contact refinements & modal popup

--- static/contact-component/contact.js
+++ static/contact-component/contact.js
@@ -11,24 +11,34 @@ function defineContact(html) {
             this.shadow.innerHTML = html;
 
             this.fields = {}; // dict with contact form fields
+            this.error = {}; // dict with contact form fields
         }
 
         connectedCallback() {
             this._set();
 
             // Onfocus change validation
-            this.shadow.querySelector('.last-name').addEventListener('blur', () => { fieldValidation(this.fields.fields.lastName, isNotEmpty); })
-            this.shadow.querySelector('.first-name').addEventListener('blur', () => { fieldValidation(this.fields.fields.firstName, isNotEmpty); })
-            this.shadow.querySelector('.message').addEventListener('blur', () => { fieldValidation(this.fields.ields.msg, isNotEmpty); })
-            this.shadow.querySelector('.email').addEventListener('blur', () => { fieldValidation(this.fields.fields.email, isEmail); })
+            this.shadow.querySelector('.last-name').addEventListener('blur', () => { fieldValidation(this.fields.lastName, this.error.lastName, true, isName); })
+            this.shadow.querySelector('.first-name').addEventListener('blur', () => { fieldValidation(this.fields.firstName, this.error.firstName, true, isName); })
+            this.shadow.querySelector('.message').addEventListener('blur', () => { fieldValidation(this.fields.msg, this.error.msg, true, isNotEmpty); })
+            this.shadow.querySelector('.email').addEventListener('blur', () => { fieldValidation(this.fields.email, this.error.email, true, isEmail); })
 
             // Handle contact form button
-            this.shadow.querySelector('.contact-btn').addEventListener('click', this._fetchContact);
+            this.shadow.querySelector('.contact-btn').addEventListener('click', this._fetchContact.bind(this));
+            this.shadow.getElementById('close').addEventListener('click', this._closePopup.bind(this));
         }
 
         disconnectedCallback() {
-            this.shadow.querySelector('.contact-btn').removeEventListener('click', this._fetchContact);
+            this.shadow.querySelector('.last-name').removeEventListener('blur', fieldValidation)
+            this.shadow.querySelector('.first-name').removeEventListener('blur', fieldValidation)
+            this.shadow.querySelector('.message').removeEventListener('blur', fieldValidation)
+            this.shadow.querySelector('.email').removeEventListener('blur', fieldValidation)
 
+            this.shadow.querySelector('.contact-btn').removeEventListener('click', this._fetchContact);
+        }
+
+        _closePopup() {
+            this.shadow.getElementById('modal').classList.add('closed');
         }
 
         _set() {
@@ -37,37 +47,59 @@ function defineContact(html) {
             this.fields.email = this.shadow.querySelector('.email');
             this.fields.firstName = this.shadow.querySelector('.first-name');
             this.fields.lastName = this.shadow.querySelector('.last-name');
+
+            this.error.msg = this.shadow.getElementById('message');
+            this.error.email = this.shadow.getElementById('email');
+            this.error.firstName = this.shadow.getElementById('first-name');
+            this.error.lastName = this.shadow.getElementById('last-name');
+
+            this.modal = this.shadow.getElementById('modal');
         }
 
-        _fetchContact() {
-            if (this._isValid()) {
-                fetch('/contact', {
-                    method: 'post',
-                    headers: new Headers({ 'Content-Type': 'application/json' }),
-                    body: JSON.stringify({
-                        firstname: this.fields.firstName.value,
-                        lastname: this.fields.lastName.value,
-                        email: this.fields.email.value,
-                        msg: this.fields.msg.value,
-                    })
-                })
-                    .then(res => res.json())
-                    .then(data => {
-                        console.log(data)
-                    })
-            }
-        }
-
-        // Validate all fields
         _isValid() {
             var valid = true;
 
-            valid &= fieldValidation(this.fields.lastName, isNotEmpty);;
-            valid &= fieldValidation(this.fields.firstName, isNotEmpty);;
-            valid &= fieldValidation(this.fields.msg, isNotEmpty);;
-            valid &= fieldValidation(this.fields.email, isEmail);
+            valid &= fieldValidation(this.fields.lastName, this.error.lastName, true, isName);
+            valid &= fieldValidation(this.fields.firstName, this.error.firstName, true, isName);
+            valid &= fieldValidation(this.fields.msg, this.error.msg, true, isNotEmpty);
+            valid &= fieldValidation(this.fields.email, this.error.email, true, isEmail);
 
             return valid;
+        }
+
+        _checkResponse(data) {
+            if (data.status === 200) {
+                this.modal.querySelector('.modal-title').innerHTML = 'Thanks for contacting me!';
+                this.fields.msg.value = '';
+                this.fields.email.value = '';
+                this.fields.firstName.value = '';
+                this.fields.lastName.value = '';
+            } else {
+                this.modal.querySelector('.modal-title').innerHTML = 'Oops, something went wrong!';
+            }
+
+            this.modal.classList.remove('closed');
+        }
+
+        _fetchContact() {
+            if (!this._isValid()) return false;
+
+            fetch('/contact', {
+                method: 'post',
+                headers: new Headers({ 'Content-Type': 'application/json' }),
+                body: JSON.stringify({
+                    firstname: this.fields.firstName.value,
+                    lastname: this.fields.lastName.value,
+                    email: this.fields.email.value,
+                    msg: this.fields.msg.value,
+                })
+            })
+                .then(data => {
+                    this._checkResponse(data);
+                })
+                .catch(err => {
+                    const mute = err
+                });
         }
     }
     customElements.define('contact-component', Contact);
@@ -85,13 +117,29 @@ function isNotEmpty(value) {
     return (value.length > 0);
 }
 
-// Function to validate given field with a given function validator
-function fieldValidation(field, validationFunction) {
-    if (field == null) return false;
-    let error = document.getElementById(field.className);
+// Name validation
+function isName(name) {
+    let regex = /^([ \u00c0-\u01ffa-zA-Z'\-])+$/;
+    return regex.test(String(name).toLowerCase());
+}
 
-    let isFieldValid = validationFunction(field.value)
-    if (!isFieldValid) {
+// Function to validate given field with a given function validator
+function fieldValidation(field, error, required, _validate) {
+    if (field == null) return false;
+    let valid = true;
+    let errorMsg = error.innerHTML.substring(0, error.innerHTML.lastIndexOf(' '));
+
+    if (required && !isNotEmpty(field.value)) {
+        valid = false;
+        error.innerHTML = errorMsg + ' required';
+    }
+
+    if (valid && !_validate(field.value)) {
+        valid = false;
+        error.innerHTML = errorMsg + ' invalid';
+    }
+
+    if (!valid) {
         if (error.classList.contains('hidden')) {
             error.classList.remove('hidden');
             setTimeout(function () {
@@ -111,18 +159,5 @@ function fieldValidation(field, validationFunction) {
         }
     }
 
-    return isFieldValid;
+    return valid;
 }
-
-// const projectContainer = document.querySelector('.project-container');
-// projects.forEach(project => {
-//     projectContainer.innerHTML += `
-//     <div class="project-card" data-tags="#all, ${project.tags}">
-//         <img src="img/${project.image}" alt="">
-//         <div class="content">
-//             <h1 class="project-name">${project.name}</h1>
-//             <span class="tags">${project.tags}</span>
-//         </div>
-//     </div>
-//     `;
-// })
