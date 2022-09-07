86fedba0437cd055754233814eb183d8e3f4c135
Iurii Kondrakov <55366304+Deezzir@users.noreply.github.com>
2022-09-07 12:06:43 -0400

    stmp init fixes

--- main.go
+++ main.go
@@ -176,7 +176,6 @@ func main() {
 	}
 
 	log.Println("[INFO]: SMTP email at", username)
-	log.Println("[INFO]: SMTP email at", password)
 	log.Println("[INFO]: Starting server at", server_port)
 	if err := http.ListenAndServe(server_port, nil); err != nil {
 		log.Fatalln(err)
